e9b1450424c54fefbca5575c8a8036b295228568
sameemul-haque <samhaqk@gmail.com>
2024-03-06 23:53:19 +0530

    feat: extract audio from video input

---
+++ main.py
@@ -0,0 +1,15 @@
+import ffmpeg
+
+input_video = "input.mp4"
+input_video_name = input_video.replace(".mp4", "")
+
+def extract_audio():
+    extracted_audio = f"audio-{input_video_name}.wav"
+    stream = ffmpeg.input(input_video)
+    stream = ffmpeg.output(stream, extracted_audio)
+    ffmpeg.run(stream, overwrite_output=True)
+    return extracted_audio
+
+def run():
+    extracted_audio = extract_audio()
+run()
